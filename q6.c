ce1005f552408736f0b9d2536776c2820cd11ba5
Gabriel de Melo Evangelista <gme@uhwc05.windows.cin.ufpe.br>
2019-10-10 10:58:52 -0300

    q6

--- q6.c
+++ q6.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
+
 #define TAM_buffer 100
 #define nThreads 5
 
@@ -12,7 +14,8 @@ typedef struct parametro{
 
 pthread_mutex_t mutexes[TAM_buffer];
 pthread_mutex_t mutexFila;
-pthread_cond_t cheio = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cheio;
+pthread_cond_t result[TAM_buffer];
 
 int bufferRes[TAM_buffer];
 parametro bufferFila[TAM_buffer];
@@ -22,9 +25,10 @@ int amount = 0;
 void *funexec(void * a){
     parametro b = *(parametro *)a;
     pthread_mutex_lock(&mutexes[b.id]);
-    printf("ca %i %i\n", b.id, b.valor);
     bufferRes[b.id] = b.valor + 10;
+    pthread_cond_signal(&result[b.id]);
     pthread_mutex_unlock(&mutexes[b.id]);
+
     pthread_exit(NULL);
 }
 
@@ -32,7 +36,9 @@ int pegarResultadoExecucao(int id){
     int ret;
     pthread_mutex_lock(&mutexes[id]);
 
-    printf("valor %i\n", bufferRes[id]);
+    while(bufferRes[id] == -1){
+        pthread_cond_wait(&result[id], &mutexes[id]);
+    }
     ret = bufferRes[id];
 
     pthread_mutex_unlock(&mutexes[id]);
@@ -45,10 +51,10 @@ int agendarExecucao(void*para){
     
     prm.id = contadorBuffer;
     bufferFila[contadorBuffer] = prm;
-    printf("%i\n", prm.id);
     contadorBuffer++;
-    amount++;
+    
     pthread_cond_signal(&cheio);
+
     pthread_mutex_unlock(&mutexFila);
     return prm.id;
 }
@@ -58,13 +64,11 @@ void *despacha(){
     int atual = 0;
     while(1){
         pthread_mutex_lock(&mutexFila);
-        if(atual >= amount){
-            printf("testa\n");
+        if(atual >= contadorBuffer){
             pthread_cond_wait(&cheio, &mutexFila);
         }
         pthread_create(&threads[contador], NULL, bufferFila[atual].func_ptr, (void*)&bufferFila[atual]);
         contador++;
-        printf("nem chega %i\n", atual);
         atual++;
         pthread_mutex_unlock(&mutexFila);
     }
@@ -72,15 +76,14 @@ void *despacha(){
 int main(){
     pthread_t despachante;
     parametro teste;
-    
+    memset(bufferRes, -1, 100);
     int i;
     pthread_create(&despachante, NULL, despacha, NULL);
-    for(i = 0; i<4; i++){
+    for(i = 0; i<26; i++){
         teste.func_ptr = funexec;
         teste.valor = i;
         teste.id = agendarExecucao(&teste);
-        pthread_cond_signal(&cheio);
-        printf("tinha isso la %i\n", pegarResultadoExecucao(teste.id));
+        printf("result %i\n", pegarResultadoExecucao(teste.id));
     }
     pthread_join(despachante, NULL);
     pthread_exit(NULL);
